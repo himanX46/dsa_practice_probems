ba2e1749d3abdb5ea9d1ddc2f1e22e8478418e51
himanbayan <nottheusualmachine@gmail.com>
2026-02-24 15:34:22 +0530

    trees

--- src/neetcode/arrays/Anagram.java
+++ src/neetcode/arrays/Anagram.java
@@ -5,49 +5,17 @@ import java.util.Map;
 
 public class Anagram {
     public static void main(String[] args) {
-        String s = "ggii";
-        String t = "eekk";
+        String s = "jar";
+        String t = "jam";
         System.out.println(isAnagram(s,t));
     }
     public static boolean isAnagram(String s, String t) {
-        /*
-        Map<Character,Integer> hmap = new HashMap<>();
-        Map<Character,Integer> hmapt = new HashMap<>();
-        int lens = s.length();
-        int lent = t.length();
-        if(lens!=lent){
-            return false;
-        }
-        for(int i=0;i<lens;i++){
-            if(hmap.containsKey(s.charAt(i))){
-                hmap.put(s.charAt(i),hmap.get(s.charAt(i))+1);
-            }else{
-                hmap.put(s.charAt(i),1);
-            }
-        }
-        for(int i=0;i<lent;i++){
-            if(hmapt.containsKey(t.charAt(i))){
-                hmapt.put(t.charAt(i),hmapt.get(t.charAt(i))+1);
-            }else{
-                hmapt.put(t.charAt(i),1);
-            }
-        }
-        for(int i=0;i<lent;i++){
-            if(!hmap.containsKey(t.charAt(i))){
-                return false;
-            }
-            if(hmap.get(t.charAt(i)) != hmapt.get(t.charAt(i))){
-                return false;
-            }
-        }
-        return true;
-
-         */
         if(s.length()!=t.length()){
             return false;
         }
         int[] freq = new int[26];
         for(int i=0;i<s.length();i++){
+            System.out.println(i+":: "+s.charAt(i)+", "+t.charAt(i));
             freq[s.charAt(i) - 'a']++;
             freq[t.charAt(i) - 'a']--;
         }
--- src/neetcode/systemDesign/MergeSortedLists.java
+++ src/neetcode/systemDesign/MergeSortedLists.java
@@ -9,47 +9,55 @@ public class MergeSortedLists {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
     public static void main(String[] args) {
-        ListNode[] lists = new ListNode[3];
+        ListNode[] lists = new ListNode[2];
         LinkedList<ListNode> l1 = new LinkedList<>();
-        LinkedList<ListNode> l2 = new LinkedList<>();
-        LinkedList<ListNode> l3 = new LinkedList<>();
-        ListNode n1 = new ListNode(1);
-        n1.next = new ListNode(4);
-        n1.next.next = new ListNode(5);
+        //LinkedList<ListNode> l2 = new LinkedList<>();
+        //LinkedList<ListNode> l3 = new LinkedList<>();
+        ListNode n1 = new ListNode(-2);
+        n1.next = new ListNode(-1);
+        n1.next.next = new ListNode(-1);
+        n1.next.next.next = new ListNode(-1);
         l1.add(n1);
         ////
-        ListNode n2 = new ListNode(1);
+        /*ListNode n2 = new ListNode(1);
         n2.next = new ListNode(3);
         n2.next.next = new ListNode(4);
-        l2.add(n2);
+        l2.add(n2);*/
         ////
-        ListNode n3 = new ListNode(2);
+        /*ListNode n3 = new ListNode(2);
         n3.next = new ListNode(6);
-        l3.add(n3);
+        l3.add(n3);*/
         lists[0] = l1.get(0);
-        lists[1] = l2.get(0);
-        lists[2] = l3.get(0);
-        mergeKLists(lists);
+        lists[1] = new ListNode();
+        //lists[1] = l2.get(0);
+        //lists[2] = l3.get(0);
+        ListNode head = mergeKLists(lists);
+        while(head!=null){
+            System.out.print(head.val+"->");
+            head = head.next;
+        }
     }
     static ListNode mergeKLists(ListNode[] lists){
+        if(lists==null || lists.length==0){
+            return null;
+        }
         int size = lists.length;
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for(int i=0;i<size-1;i++){
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>((a,b)->Integer.compare(a.val,b.val));
+        for(int i=0;i<size;i++){
+            System.out.println(lists[i].val);
             ListNode head = lists[i];
             while(head!=null){
-                minHeap.add(head.val);
+                minHeap.add(head);
                 head = head.next;
             }
         }
-        LinkedList<ListNode> result = new LinkedList<>();
         ListNode head = new ListNode(-99);
-        ListNode tail = new ListNode(-99);
-        for(int i=0;i<minHeap.size();i++){
-            ListNode newNode = new ListNode(minHeap.poll());
-            tail.next = newNode;
+        ListNode tail = head;
+        while(!minHeap.isEmpty()){
+            ListNode smallest = minHeap.poll();
+            tail.next = smallest;
+            tail = tail.next;
         }
-        tail.next = null;
-        head = head.next;
-        return head;
+        return head.next;
     }
 }
