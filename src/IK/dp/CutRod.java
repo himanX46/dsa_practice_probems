5991e5d597e2b6ecfa158290f168d5b5c6c063b8
himanbayan <nottheusualmachine@gmail.com>
2026-03-26 21:45:14 +0530

    dp

--- src/IK/dp/CutRod.java
+++ src/IK/dp/CutRod.java
@@ -4,10 +4,9 @@ public class CutRod {
     public static void main(String[] args) {
         int n=4;
         int[] cuts = {1,5,8,9};
-        System.out.println(helper(n,cuts));
+        System.out.println(helper_dp(n,cuts));
     }
     static int helper(int n, int[] cuts){
-        int dp[] = new int[n+1];
         if(n==0){
             return 0;
         }
@@ -19,5 +18,19 @@ public class CutRod {
             }
         }
         return maxRev;
+    }
+
+    static int helper_dp(int n, int[] cuts){
+        int[] dp = new int[n+1];
+        for(int i=1;i<=n;i++){
+            int maxRev = 0;
+            for(int j=0;j<cuts.length;j++){
+                if (cuts[j] <= i) {
+                    maxRev = Math.max(maxRev, cuts[j] + dp[i - cuts[j]]);
+                }
+            }
+            dp[i] = maxRev;
+        }
+        return dp[n];
     }
 }
